7a27503a05be7fbdda2137b5d152a9e06bca0cdb
Jitesh  Yadav <jituu098@gmail.com>
2026-08-09 16:39:28 +0530

    Solved: Set Mismatch (Easy)

--- Sorting/645-set-mismatch.py
+++ Sorting/645-set-mismatch.py
@@ -1,5 +1,10 @@
-class Solution {
-    public int[] findErrorNums(int[] nums) {
-        
+        int start = 0;
+    void cyclicSort(int[] nums){
+        return new int[] {-1,-1};
     }
-}
+        }
+            }
+                return new int[] {nums[i], i + 1};
+            if (nums[i] != i+1){
+        for (int i = 0; i < nums.length; i++){
+        cyclicSort(nums);
