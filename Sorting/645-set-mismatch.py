024cefa3836fe5b60c9d0bdf136dad79c5ce6cac
Jitesh  Yadav <jituu098@gmail.com>
2026-08-09 16:59:52 +0530

    Delete Sorting/645-set-mismatch.py

--- Sorting/645-set-mismatch.py
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] findErrorNums(int[] nums) {
-        cyclicSort(nums);
-        for (int i = 0; i < nums.length; i++)
-        {
-            if (nums[i] != i+1){
-                return new int[] {nums[i], i 
-                + 1};
-            }
