04227523e7cefc820d34a6c2f7f01a1df48a17f8
Jitesh  Yadav <jituu098@gmail.com>
2026-08-10 20:33:07 +0530

    Solved: First Missing Positive (Hard)

--- Hash-Table/41-first-missing-positive.java
+++ Hash-Table/41-first-missing-positive.java
@@ -1,5 +1,23 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        
+        int start =0;
+        while(start < nums.length){
+            int correct  = nums[start] - 1;
+            if (nums[start] > 0 && nums[start] <= nums.length && nums[start] != nums
+            [correct]){
+                // swap
+                int temp = nums[start];
+                nums[start] = nums[correct];
+                nums[correct] = temp;
+            } else{
+                start ++;
+            }
+        }
+        for (int i=0; i < nums.length; i++){
+            if (nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
     }
 }
