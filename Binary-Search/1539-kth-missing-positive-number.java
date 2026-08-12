8da9c39b5a2f445b459be85cf33f393fa6708897
Jitesh  Yadav <jituu098@gmail.com>
2026-08-12 12:26:15 +0530

    Solved: Kth Missing Positive Number (Easy)

--- Binary-Search/1539-kth-missing-positive-number.java
+++ Binary-Search/1539-kth-missing-positive-number.java
@@ -2,4 +2,12 @@ class Solution {
     public int findKthPositive(int[] arr, int k) {
         
     }
+    int BS(int[]arr, int target){
+        int start = 0;
+        int end = arr.length;
+        while(start < end){
+            int mid = start + (end-start)/2;
+            if ()
+        }
+    }
 }
